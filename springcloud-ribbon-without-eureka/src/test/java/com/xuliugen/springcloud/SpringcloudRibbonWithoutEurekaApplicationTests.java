e1c3cbee8347c458e94229096e7e326fd25142ae
xuliugen <1573876303@qq.com>
2017-07-27 09:50:15 +0800

    删除springcloud-user2，因为可以一个项目运行多个实例

--- springcloud-ribbon-without-eureka/src/test/java/com/xuliugen/springcloud/SpringcloudRibbonWithoutEurekaApplicationTests.java
+++ springcloud-ribbon-without-eureka/src/test/java/com/xuliugen/springcloud/SpringcloudRibbonWithoutEurekaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringcloudUser2ApplicationTests {
+public class SpringcloudRibbonWithoutEurekaApplicationTests {
 
 	@Test
 	public void contextLoads() {
--- springcloud-user/src/main/java/com/xuliugen/springcloud/UserApplication.java
+++ springcloud-user/src/main/java/com/xuliugen/springcloud/UserApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SpringcloudUser2Application {
+public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudUser2Application.class, args);
+        SpringApplication.run(UserApplication.class, args);
     }
 }
