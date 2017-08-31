eba9c94a6c2249929681c58b7df9be46e3971875
xuliugen <1573876303@qq.com>
2017-08-31 16:49:46 +0800

    不配置账户密码情况下的访问

---
+++ springcloud-config/springcloud-config-client-security/src/main/java/com/xuliugen/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package com.xuliugen.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by xuliugen on 2017/8/31.
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping(value = "profile")
+    public String getProfile() {
+        return profile;
+    }
+}
